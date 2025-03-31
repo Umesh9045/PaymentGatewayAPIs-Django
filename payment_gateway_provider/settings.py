8ed033a89f047cce8ade1e77674687727a843a4f
Umesh9045 <umeshsonawane9045@gmail.com>
2025-03-31 17:53:32 +0530

    added app and models for payment_method_app, and vendor_app

--- payment_gateway_provider/settings.py
+++ payment_gateway_provider/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'payment_methods_app',
+    'vendor_app',
 ]
 
 MIDDLEWARE = [
